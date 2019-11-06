1c9528deab6204f2a41e4ab5cfd9a48eed90c05f
Alena <mouse.programmouse@gmail.com>
2019-11-06 13:40:03 -0800

    init TaskCard componrnt

--- frontend/todo/src/App.js
+++ frontend/todo/src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import TaskCard from './TaskCard';
 import './App.css';
 
+const data = [
+  {
+      "TimeSt": "2019-09-28T17:12:51.346962-07:00",
+      "description": "Add documentation for each http-method.",
+      "id": "dd8797c8-8af4-4683-ab93-38db1cf408d9",
+      "project": "ToDo API"
+  },
+  {
+      "TimeSt": "2019-09-28T17:12:51.346962-07:00",
+      "description": "Add documentation for each http-method.",
+      "id": "dd8797c8-8af4-4683-ab93-38db1cf408d9",
+      "project": "ToDo API"
+  }
+];
+
 function App() {
+
   return (
     <div className="App">
-    TODO
+      <TaskCard />
     </div>
   );
 }
---
+++ frontend/todo/src/TaskCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TaskCard = (props) => {
+
+  return (
+    <div className="card">
+      <h3>ToDo:</h3>
+      <p>Description</p>
+    </div>);
+}
+
+export default TaskCard;
