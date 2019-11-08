f3c1794d4f7fe19ced027acaca70a97a69d379a2
Alena <mouse.programmouse@gmail.com>
2019-11-07 16:30:30 -0800

    add ReactTeastUtils add-on. Write first test✨

---
+++ frontend/todo/src/TaskCard.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactTestUtils from 'react-dom/test-utils';
+import TaskCard from './TaskCard';
+
+describe("TaskCard", function() {
+
+  it ("can render without error", function() {
+    let component, element;
+
+    element = React.createElement(
+      'TaskCard'
+      //{} //props
+      //add children as the last argument
+    );
+
+    // Render into a document fragment and return the full component instance.
+    expect(function() {
+      component = ReactTestUtils.renderIntoDocument(element);
+    }).not.toThrow();
+  });
+});
