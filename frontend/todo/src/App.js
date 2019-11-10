19930072c25cf78e970b60f1316fd3ea103964e0
Alena <mouse.programmouse@gmail.com>
2019-11-10 10:17:13 -0800

    Change componentWillMount to componentDidMount.  componentWillMount has been renamed, and is not recommended for use.

--- frontend/todo/src/App.js
+++ frontend/todo/src/App.js
@@ -5,7 +5,12 @@ import './App.css';
 class App extends Component {
 
   state = {
-    tasksList : [{
+    tasksList: []
+  };
+
+  componentDidMount = () => {
+    //get and set data for tasks' list
+    this.setState({ tasksList: [{
           TimeSt: "2019-09-28T17:12:51.346962-07:00",
           description: "Add documentation for each http-method.",
           id: "dd8797c8-8af4-4683-ab93-38db1cf408d9",
@@ -16,11 +21,45 @@ class App extends Component {
           description: "Add documentation for each http-method.",
           id: "dd8797c8-8af4-4683-ab93-38db1cf408d9",
           project: "ToDo API"
-      }]
-  };
+      }]});
+  }
 
-  componentWillMount = () => {
-    //get and set data for tasks' list
+  //update task by id
+  updateTaskById = (id, newTitle, newDescription) => {
+    let newTasksList = this.state.tasksList;
+    //find and update task
+
+    //set new state
+    this.setState ({
+      tasksList: newTasksList
+    });
+  }
+
+  //delete task by id
+  deleteTaskById = (id) => {
+    this.setState ({
+      tasksList: this.state.tasksList.filter(task => task.id!==id)
+    });
+  }
+
+  addNewTask = (title, description) => {
+    let newTasksList = this.state.tasksList;
+    //add task if title and description != ''
+
+    //set new state
+    this.setState ({
+      tasksList: newTasksList
+    });
+  }
+
+  sortTasksByDate = () => {
+    let newTasksList = this.state.tasksList;
+    //sort array
+
+    //set new state
+    this.setState ({
+      tasksList: newTasksList
+    });
   }
 
   render () {
