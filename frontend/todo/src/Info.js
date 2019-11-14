1de2b594f3b55bdf7e46f2f649d96fdc252bfd5e
Alena <mouse.programmouse@gmail.com>
2019-11-13 20:38:00 -0800

    add props to Info component

--- frontend/todo/src/Info.js
+++ frontend/todo/src/Info.js
@@ -3,10 +3,19 @@ import PropTypes from 'prop-types'
 
 const Info = (props) => {
 
+  const { srcImg, header, instruction, link } = props;
+
   return (
     <section id="info" className="info">
     </section>
   );
+}
+
+Info.propTypes = {
+  srcImg: PropTypes.string.isRequired,
+  header: PropTypes.string.isRequired,
+  instruction: PropTypes.object,
+  link: PropTypes.string.isRequired,
 }
 
 export default Info
